64be6b09f16fbe0a32d878a482f61f8884f8f080
NIKHIL-codes00 <144558418+NIKHIL-codes00@users.noreply.github.com>
2025-01-20 19:50:19 +0000

    comparisions

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,36 @@
+// console.log(2 == 2);
+// console.log( 2 != 5 );
+// console.log( 3 > 5 );
+// console.log( 3 >= 7);
+// console.log( 3 <= 9);
+// console.log( 1 < 3 );
+
+console.log( "2" >= 1);// not predictable output and hence we should avoid comparing different datatypes
+console.log( "03" > 2);// TypeScript does not allow this type of comparisions
+
+console.log(null > 0 );
+console.log( null == 0 );
+console.log( null >= 0 );
+// The reason is that an equality check and comparisions work differently, comparisions convert null to a number treating as 0
+console.log( undefined > 0 );
+console.log( undefined < 0 );
+console.log( undefined == 0 );
+
+// We avoid ambigiuos types of comparisions for clean code
+// === Strict Check
+console.log( "2" === 2 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
