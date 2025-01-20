6f51682d910a6dd122c0ffeebe5f4233e0748382
NIKHIL-codes00 <144558418+NIKHIL-codes00@users.noreply.github.com>
2025-01-20 20:40:17 +0000

    Summary of datatypes

---
+++ 01_basics/05_dataTypes_summary.js
@@ -0,0 +1,41 @@
+// Primitive
+// 7Types : String, Number, Boolean, null, undefined, symbol, BigInt
+const score = 100
+const scorePrecise = 100.5
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id);
+console.log(anotherId);
+console.log( id == anotherId );
+
+const bigNum = 190484738397928777777777777777777773n
+
+
+// Non-Primitive or Reference type
+// Array, objects, functions
+ 
+const heros = ["shaktiman", "gangadhar", "bheem"]
+let myObj={
+    name: "nikhil",
+    age: 21,
+}
+const myFunction=function(){
+console.log("HELLO WORLD");
+}
+
+console.log(typeof bigNum);
+console.log(typeof myFunction);
+console.log(typeof myObj);
+console.log(typeof heros);
+console.log(typeof id);
+// All non-primitive types have object datatype and for function also known as function object
+
+
+
+
+
+
+
