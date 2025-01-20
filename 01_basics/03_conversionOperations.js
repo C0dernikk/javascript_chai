ad0b84d8ea6f19340daa7768059dda9ac14782e6
NIKHIL-codes00 <144558418+NIKHIL-codes00@users.noreply.github.com>
2025-01-20 19:21:47 +0000

    Operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -26,6 +26,51 @@ let StringNum = String(givenNum)
 console.log(typeof StringNum);
 console.log(StringNum);
 
+// **********************  Operations ********************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(4/2);
+console.log(7%3);
+console.log(2**3);
+
+let str1 = "Hello"
+let str2 = "Nikk"
+let str3 = str1 + str2
+console.log(str3);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log( 2 + 2 + "4");
+console.log( "4" + 5 + 6);
+console.log( 3 + 4 + "7" + 1);
+// use parantheses and not think much about precedences
+console.log( (3+7) * 4 % 7);
+
+let num1, num2, num3
+num1 = num2 = num3 = 7
+let gameCounter = 10
+console.log(++gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
