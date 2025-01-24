7688250fe1c9678cf581b4b8112f668e6e95aa49
NIKHIL-codes00 <144558418+NIKHIL-codes00@users.noreply.github.com>
2025-01-24 13:55:06 +0000

    Range-[0,1)

--- 01_basics/07_Nums_and_Maths.js
+++ 01_basics/07_Nums_and_Maths.js
@@ -23,7 +23,7 @@ console.log(Math.ceil(4.2));
 console.log(Math.floor(4.6));
 console.log(Math.min(4, 7, 2, 0));
 console.log(Math.max(5, 8, 37, 3));
-console.log(Math.random());// 0 to 1 (random values)
+console.log(Math.random());// [0,1) -- (random values)
 console.log(Math.floor(Math.random()*10) + 1);// 1 to 10
 
 const min = 10
