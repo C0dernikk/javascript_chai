431d53afef1bab7f04fa2d66e9cf5b823f4cdf9d
NIKHIL-codes00 <144558418+NIKHIL-codes00@users.noreply.github.com>
2025-01-27 04:02:37 +0000

    Destructuring of objects

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,44 @@
+//Destructuring of Objects
+
+const course = {
+    coursename: "JS in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+//course.courseInstructor--> Instead of using this 
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// const navbar = ({company}) => {
+// Destructuring used in React
+// }
+// navbar(company = "hitesh")
+
+//JSON(Javascript Object Notation)--> Used to store and send object(eg.api)
+
+// Object with no name
+
+// {
+// "name": "nikk",
+// "gender": "Male"
+// }
+
+//Array mai bhi ho sakta hai --> APIs
+// [
+//     {},
+//     {},
+//     {}
+// ]
+
+
+
+
+
+
+
+
+
+
