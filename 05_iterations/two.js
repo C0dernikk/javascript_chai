cd2446457f7a679f64d9948f9975504e4fec3624
NIKHIL-codes00 <144558418+NIKHIL-codes00@users.noreply.github.com>
2025-02-01 09:46:31 +0000

    While and do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+let index = 0
+while (index <= 10) {
+    // console.log(`Value of index is ${index}`);
+    index += 3
+}
+
+let myArray = ["shinchan", "gian", "kira"]
+
+let arr_ind = 0
+while (arr_ind < myArray.length) {
+    // console.log(`Value is ${myArray[arr_ind]}`);
+    arr_ind++;
+}
+
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10);
+
